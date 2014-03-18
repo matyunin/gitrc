8bb34b4e604bcee71150c4fafa903014d72d7384
Mikhail Matyunin <free.all.bums@gmail.com>
2014-03-18 21:08:51 +0400

    Remove git-prompt caller

--- gitrc.sh
+++ gitrc.sh
@@ -6,8 +6,8 @@
 
 # Git prompt
 
-. /usr/share/git/git-prompt.sh
-PS1='\[\033[01;31m\]`__git_ps1 "%s "`\[\033[00m\]\[\033[01;32m\]\u@\h\[\033[01;34m\] \w \$ \[\033[00m\]'
+#. /usr/share/git/git-prompt.sh
+#PS1='\[\033[01;31m\]`__git_ps1 "%s "`\[\033[00m\]\[\033[01;32m\]\u@\h\[\033[01;34m\] \w \$ \[\033[00m\]'
 
 # Git aliases
 
